aecff510c4b50cf3e54a9f579f25294dd23f6a62
b1ackitty <jieunj0301@gmail.com>
2025-08-09 07:54:44 +0900

    [학습] Class vs. Type과 Interface

--- 6-classes/6.4-classes-vs-types-and-interfaces/6.4.2-exercise/exercise.ts
+++ 6-classes/6.4-classes-vs-types-and-interfaces/6.4.2-exercise/exercise.ts
@@ -19,6 +19,8 @@ class Book {
   }
 }
 
+const book = new Book('가공범', '히가시노 게이고')
+book.describe() // '이 책의 제목은 "가공범"이고, 저자는 히가시노 게이고입니다.'
 
 // 문제 2 
 // 아래 Tree 타입의 새 객체를 생성하세요.
@@ -28,6 +30,10 @@ type Tree = {
   age: number
 }
 
+const tree: Tree = {
+  height: 15,
+  age: 4
+}
 
 // 문제 3
 // 아래 Car 인터페이스의 새 객체를 생성하세요.
@@ -36,4 +42,10 @@ interface Car {
   make: string
   model: string
   year: number
+}
+
+const car: Car = {
+  make: 'KIA',
+  model: 'Sorento',
+  year: 2025
 }
