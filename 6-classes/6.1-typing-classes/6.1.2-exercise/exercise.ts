5dd423f4bc0c63d3f405902a7b4d3845befee784
b1ackitty <jieunj0301@gmail.com>
2025-08-09 07:43:56 +0900

    [학습] Class 타입

--- 6-classes/6.1-typing-classes/6.1.2-exercise/exercise.ts
+++ 6-classes/6.1-typing-classes/6.1.2-exercise/exercise.ts
@@ -7,12 +7,43 @@
 //    - lastName: string
 //    - age: number
 //    - fullName: 사람의 전체 이름을 반환하는 메서드
+class Person {
+  firstName: string
+  lastName: string
+  age: number
 
+  constructor(firstName: string, lastName: string, age: number) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+
+  fullName(): string {
+    return this.firstName + this.lastName
+  }
+}
 
 // 2. `Person` 클래스를 상속받는 `Student` 클래스를 만들고, 다음과 같은 추가 프로퍼티를 구현하세요.
 //    - studentId: number
 //    - courses: string[]
 //    - enroll: 과목 이름을 매개변수로 받아 courses 배열에 추가하는 메서드
+class Student extends Person {
+  studentId: number
+  courses: string[]
 
+  constructor(firstName: string, lastName: string, age: number, studentId: number, courses: string[]) {
+    super(firstName, lastName, age)
+    this.studentId = studentId
+    this.courses = courses
+  }
+
+  enroll(course: string): void {
+    this.courses.push(course)
+  }
+}
 
 // 3. `Student` 클래스의 인스턴스를 생성하고, 그 속성과 메서드가 어떻게 동작하는지 예시를 보여주세요.
+const student = new Student('이름', '성', 31, 152530, [ 'HTML', 'CSS', 'JavaScript' ])
+console.log(student.fullName()) // '이름성'
+student.enroll('TypeScript')
+console.log(...student.courses) // [ 'HTML', 'CSS', 'JavaScript', 'TypeScript' ]
