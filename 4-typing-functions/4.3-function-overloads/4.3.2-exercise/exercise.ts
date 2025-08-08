e25f2160b77256aaf674b97f301593bcfd313838
b1ackitty <jieunj0301@gmail.com>
2025-08-08 15:35:39 +0900

    [학습] Function 오버로드

--- 4-typing-functions/4.3-function-overloads/4.3.2-exercise/exercise.ts
+++ 4-typing-functions/4.3-function-overloads/4.3.2-exercise/exercise.ts
@@ -1,6 +1,11 @@
 // Exercise 1: Create function overloads for a function named `getLength`
 // that can take either a string or an array and return their length.
+function getLength(value: string): number
+function getLength(value: any[]): number
 
+function getLength(value: any): number {
+  return value.length
+}
 // Test your implementation:
-// getLength("TypeScript"); // 10
-// getLength([1, 2, 3, 4, 5]); // 5
+console.log(getLength("TypeScript")); // 10
+console.log(getLength([1, 2, 3, 4, 5])); // 5
