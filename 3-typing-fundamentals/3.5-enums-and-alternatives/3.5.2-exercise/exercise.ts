44d5aad30676fb7356b8d9c18e1f9594ebd03a66
b1ackitty <jieunj0301@gmail.com>
2025-08-08 14:53:33 +0900

    [학습] enum 타입과 대체 방법

--- 3-typing-fundamentals/3.5-enums-and-alternatives/3.5.2-exercise/exercise.ts
+++ 3-typing-fundamentals/3.5-enums-and-alternatives/3.5.2-exercise/exercise.ts
@@ -5,20 +5,65 @@
 
 // 1. Up, Down, Left, Right 네 가지 값을 가지는 
 //    Direction이라는 enum을 정의하세요.
-
+enum Direction {
+  Up = "up",
+  Down = "down",
+  Left = "left",
+  Right = "right"
+}
 
 // 2. Direction enum을 인자로 받아 switch문을 사용해 
 //    "위로 이동!"과 같은 문자열을 반환하는 `move` 함수를 만드세요.
-
+function move(direction: Direction) {
+  switch(direction) {
+    case "up":
+      return "위로 이동!"
+    case "down":
+      return "아래로 이동!"
+    case "left":
+      return "왼쪽으로 이동!"
+    case "right":
+      return "오른쪽으로 이동!"
+  }
+}
 
 // 3. Success = 1, Failure = -1, Pending = 0의 
 //    명시적 값을 갖는 Status라는 enum을 정의하세요.
-
+enum Status {
+  Success = 1,
+  Failure = -1,
+  Pending = 0
+}
 
 // 4. Status enum을 인자로 받아 "오퍼레이션 성공." 또는 
 //    "오퍼레이션 실패"와 같은 메시지를 반환하는 getStatusMessage 함수를 만드세요.
+function getStatusMessage(status: Status) {
+  switch(status) {
+    case 1:
+      return "오퍼레이션 성공"
+    case -1:
+      return "오퍼레이션 실패"
+    case 0:
+      return "오퍼레이션 보류"
+  }
+}
 
 // 5. 이번에는 enum 대신 객체 리터럴을 사용해서 동일한 기능을 구현하세요.
+const statusLiteral = {
+  Success: 1,
+  Failure: -1,
+  Pending: 0
+} as const
 
 // 6. statusLiteral을 인자로 받아서 
 //    해당 메시지를 반환하는 getStatusMessageLiteral 함수를 만드세요.
+function getStatusMessageLiteral(status: keyof typeof statusLiteral) {
+  switch(status) {
+    case "Success":
+      return "오퍼레이션 성공"
+    case "Failure":
+      return "오퍼레이션 실패"
+    case "Pending":
+      return "오퍼레이션 보류"
+  }
+}
